bee4bad99e5767ccd7107f5cd04cc9b4a7c27625
tanishqmanuja <tanishqmanuja@gmail.com>
2024-10-17 12:54:23 +0530

    chore: add release script

---
+++ scripts/release.ts
@@ -0,0 +1,33 @@
+import { $ } from "bun";
+import { version } from "../package.json";
+
+const WORKFLOW_ID = "release.yaml"
+
+const bump = process.argv[2] ?? "patch";
+
+const { owner, repo } = await $`git config --get remote.origin.url`.quiet().text().then(url => {
+ const [owner, repo] = url.split("/").slice(-2); 
+  if(!owner || !repo) {
+    throw new Error("Cannot find owner or repo");
+  }
+  return {
+    owner,
+    repo: repo.replace(".git", "").trim(),
+  }
+})
+
+if(bump === "major" || bump === "minor" || bump === "patch") {
+  console.log(`Current Version: ${version}`);
+  console.log(`Bump Type: ${bump}`);
+  console.log(`Repo: ${owner}/${repo}`);
+
+  console.log("\nDispatching workflow...\n");
+
+  await $`gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/${owner}/${repo}/actions/workflows/${WORKFLOW_ID}/dispatches -f "ref=main" -f "inputs[bump]=${bump}"`;
+
+  process.exit(0);
+} else {
+  console.log("Invalid bump type");
+  process.exit(1);
+}
